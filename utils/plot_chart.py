48994b5b2f9663c4cf146356ff73121e21082210
vinhdc10998 <chivinh10998@gmail.com>
2021-04-27 14:40:00 +0700

    Vinh modify eval

--- utils/plot_chart.py
+++ utils/plot_chart.py
@@ -24,7 +24,7 @@ def draw_chart(train_loss, train_r2_score, val_loss, val_r2_score, region, type_
 def draw_MAF_R2(pred, label, a1_freq_list, type_model, region, bins=30, output_prefix = 'images'):
     if not os.path.exists(output_prefix):
         os.mkdir(output_prefix)
-    bins_list = pd.cut(a1_freq_list, bins, labels=range(bins))
+    bins_list = pd.cut(a1_freq_list.tolist(), bins, labels=range(bins))
     pred_bins = [[] for _ in range(bins)]
     label_bins = [[] for _ in range(bins)]
     for index, bin in enumerate(bins_list):
