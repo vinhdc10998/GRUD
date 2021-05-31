0de85cd1c27cb1ea01fea3c179b2544a731088eb
Dương Chí Vinh <55987491+vinhdc10998@users.noreply.github.com>
2021-05-31 16:53:58 +0700

    Update train.py
    fix learning rate

--- train.py
+++ train.py
@@ -57,6 +57,8 @@ def run(dataloader, model_config, args, region):
         checkpoint = torch.load(filename)
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        print("Old learing rate:",optimizer.param_groups[0]['lr'])
+        optimizer.param_groups[0]['lr'] = lr
         start_epochs = checkpoint['epoch']+1
 
     for epoch in range(start_epochs, epochs+1):
